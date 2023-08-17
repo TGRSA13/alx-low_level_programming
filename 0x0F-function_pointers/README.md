14f81939d6dc5f9a37e21c5b35b0bdeb3503eb21
TGRSA13 <tgmchunu@gmail.com>
2023-08-17 10:11:37 -0700

    0x0F.C

--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,2 @@
+Project
 0x0F. C - Function pointers
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
-#define FUNCTION_OINTERS_H
+#define FUNCTION_POINTERS_H
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -7,6 +7,6 @@
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int ^array, int size, int (^cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
